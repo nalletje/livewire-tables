fb5bffffa3458f7d3f6aa9b68ef376f23cb54fc3
Quirinus <n4l-gaming@live.nl>
2023-09-12 19:45:36 +0200

    [add] Row button href target

--- resources/views/partials/row-buttons.blade.php
+++ resources/views/partials/row-buttons.blade.php
@@ -1,7 +1,10 @@
 <div class="d-flex flex-wrap justify-content-center gap-2 float-end">
     @foreach($buttons as $button)
         @if (! isset($button['permission']) || \Illuminate\Support\Facades\Auth::user()->can($button['permission']))
-        <a href="{{ route($button['route'], $routeParam) }}" class="btn btn-xs btn-outline-primary">
+        <a href="{{ route($button['route'], $routeParam) }}"
+           class="btn btn-xs btn-outline-primary"
+           @if(isset($button['target'])) target="{{ $button['target'] }}" @endif
+        >
             @if(isset($button['icon'])) <i class="{{ $button['icon'] }}"></i> @endif {{ $button['label'] ?? '' }}
         </a>
         @endif
